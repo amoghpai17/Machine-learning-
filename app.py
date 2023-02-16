1711b228e8cd6df15cec639ed002e6c6d3dedbd2
Amogh Pai <amoghpai@Amoghs-MacBook-Air.local>
2023-02-16 15:26:35 +0530

    sending changes to gitub

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+app=Flask(__name__)
+
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return "Starting MAchine Learning Project"
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+     
+app.run(host='0.0.0.0', port=8080)
